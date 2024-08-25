140060f255e01c97bc6fb103dd2c3b96f324da52
Kayode <drkay4sure@gmail.com>
2024-08-26 00:08:28 +0100

    first try

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+res.status(201).send({msg: "Welcome Kay!"})
+})
+
+const mockUsers = [
+    { id: 1, name: "john", username: "john123" },
+    { id: 2, name: "ben", username: "ben123" },
+    { id: 3, name: "hope", username: "hope123" },
+    { id: 4, name: "kay", username: "kay234" },
+]
+app.get('/api/users', (req, res) => {
+    res.send(mockUsers)
+})
+
+app.get('/api/users/:id', (req, res) => {
+    console.log(req.params);
+    const parsedID = parseInt(req.params.id)
+
+    if (isNaN(parsedID)) 
+        return res.status(400).send({ msg: "Bad request. Invalid ID!!!"})
+
+    const findUser = mockUsers.find((user) => {
+        user.id === parsedID
+    }) 
+})
+
+app.get("/api/v1/products", (req, res) => {
+    res.status(202).send([
+        { id: 1, product: "Gucci Bag", price: "$100"},
+        { id: 2, product: "Nike Shoes", price: "$150"},
+        { id: 3, product: "Adidas Shoes", price: "$120"},
+        { id: 4, product: "Puma Shoes", price: "$180"},
+        { id: 5, product: "Reebok Shoes", price: "$160"},
+    ])
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
